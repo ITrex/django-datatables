f9dcbdc3984893806baa1069300c2f49e9f98249
Renat Galimov <renat2017@gmail.com>
2014-07-24 11:28:54 +0400

    Initial release. Versioin 1.10.1

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Django package for DataTables:
+able enhancing plug-in for the jQuery Javascript library'''
+
+from setuptools import setup
+
+setup(
+    name='django-datatables',
+    version='1.10.1',
+    url='http://datatables.com',
+    description=globals()['__doc__'],
+    author='Jaime Pillora',
+    maintainer='Renat Galimov',
+    maintainer_email='renat2017@gmail.com',
+    license='MIT License',
+    keywords=['django', 'datatables'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['django_datatables'],
+    package_data={'django_datatables': ['static/django_datatables/js/*',
+                                        'static/django_datatables/css/*',
+                                        'static/django_datatables/images/*']}
+)
